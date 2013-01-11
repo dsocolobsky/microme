9dd3240de9dd649e7a47d3943a42c5d7e9639a4a
Dylan Socolobsky <dysoco97@gmail.com>
2013-01-11 14:52:11 -0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microme::Application.config.secret_token = 'c1e39bb8dfd527503a2c6cd0aadc0f6b38fb25e8fe78974afaf7b8fab49fb76abe1dbccf17acc6170376a5a0026f9c882a918c2a41829ea51196b516ff2f6cf0'
